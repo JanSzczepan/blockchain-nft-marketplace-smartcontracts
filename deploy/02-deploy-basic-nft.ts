8fd9cbf2eaf6af8dd4c07ab226b6d0585dc985b5
Jan Szczepan <janszczepan62@gmail.com>
2023-05-25 22:24:40 +0200

    BasicNft contract and deploy script

---
+++ deploy/02-deploy-basic-nft.ts
@@ -0,0 +1,32 @@
+import { DeployFunction } from 'hardhat-deploy/dist/types'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { developmentChains, networkConfig } from '../helper-hardhat-config'
+import verify from '../utils/verify'
+
+const deployBasicNft: DeployFunction = async function (
+   hre: HardhatRuntimeEnvironment
+) {
+   const { deployments, getNamedAccounts, network } = hre
+   const { deploy } = deployments
+   const { deployer } = await getNamedAccounts()
+   const chainId = network.config.chainId!
+
+   const args: any[] = []
+
+   const basicNft = await deploy('BasicNft', {
+      from: deployer,
+      args,
+      log: true,
+      waitConfirmations: networkConfig[chainId].blockConfirmations || 1,
+   })
+
+   if (
+      developmentChains.includes(network.name) &&
+      process.env.ETHERSCAN_API_KEY
+   ) {
+      await verify(basicNft.address, args)
+   }
+}
+
+deployBasicNft.tags = ['all', 'nftmarketplace']
+export default deployBasicNft
